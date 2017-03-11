030dbcfa360b06af1c18912554c24b81d0aafbd4
dcsaxcs <895909949@qq.com>
2017-03-11 15:46:08 +0800

    send Async Request

--- app/src/main/java/com/itmeima/ling/myapplication/MainActivity.java
+++ app/src/main/java/com/itmeima/ling/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.ListView;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -22,7 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mListView = (ListView) findViewById(R.id.lv);
       //  ButterKnife.bind(this);
-        sendSyncRequest();
+      //  sendSyncRequest();
+        sendAsyncRequest();
+    }
+
+    private void sendAsyncRequest() {
+
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                //创建网络请求
+                String url = "http://gank.io/api/data/福利/10/1";
+                Request request = new Request.Builder().get().url(url).build();
+                //异步请求,不需要等待网络返回,及执行后面的代码
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String result = response.body().string();
+                        Log.d(TAG, "onResponse: "+result);
+                    }
+                });
+        Log.d(TAG, "sendAsyncRequest: ");
 
     }
 
